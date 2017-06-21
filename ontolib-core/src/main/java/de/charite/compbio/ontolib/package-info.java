18a40d0f79c28d90020223b767a8c0ae98cfaf99
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2017-06-21 22:23:45 +0200

    Removing issue list from docstring.

--- ontolib-core/src/main/java/de/charite/compbio/ontolib/package-info.java
+++ ontolib-core/src/main/java/de/charite/compbio/ontolib/package-info.java
@@ -24,27 +24,6 @@
  * <code>de.charite.compbio.utils</code> -- Utility code used in the library.
  * </p>
  *
- * <h5>Open Questions / TODOs</h5>
- * 
- * <ul>
- * <li>Support for ZPO missing, is there a working zp.obo file?</li>
- * <li>Updates for Uberpheno/uPheno available?</li>
- * <li>Which namespace do we want?</li>
- * <li>Tests for ontolib-core module</li>
- * <li>Check coverage of ontolib-io module</li>
- * <li>Further split out ontology-specific parts into their own Maven artifacts?</li>
- * <li>Will need better project-wide documentation, can do with <tt>mvn site</tt>?</li>
- * <li>So far no annotation of ZPO and Upheno to genes</li>
- * <li>We need to flesh out the different parsing modes for the ontologies (maybe more than one
- * mode, e.g., "slim" and "fat"?) and consolidate the copy and paste code</li>
- * <li>We should implement BOQCA?!</li>
- * <li>Provide CLI tools in ontolib-cli package, will require more library support, at least
- * versionized serialization: Resnik precomputation + serialization, score distribution
- * precomputation + serialization, matching term sets to "world object", e.g., disease or gene
- * annotation with multi-testing correction, loading ontologies and serialization</li>
- * <li>Resolve all embedded TODO comments!</li>
- * </ul>
- *
  * @author <a href="mailto:manuel.holtgrewe@bihealth.de">Manuel Holtgrewe</a>
  * @author <a href="mailto:sebastian.koehler@charite.de">Sebastian Koehler</a>
  */
