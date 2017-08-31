e070babee93ebb2d35e2ed718cdc00118d7169f9
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2017-08-31 22:37:59 +0200

    Making parsers more robust.

--- ontolib-io/src/test/java/com/github/phenomics/ontolib/io/obo/hpo/HpoNegativeDiseaseAnnotationParserTest.java
+++ ontolib-io/src/test/java/com/github/phenomics/ontolib/io/obo/hpo/HpoNegativeDiseaseAnnotationParserTest.java
@@ -12,7 +12,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-public class HpoDiseaseAnnotationParserTest {
+public class HpoNegativeDiseaseAnnotationParserTest {
 
   @Rule
   public TemporaryFolder tmpFolder = new TemporaryFolder();
