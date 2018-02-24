48866e595d8489a768c0afa65f135385f535c7fd
pnrobinson <peter.robinson@charite.de>
2018-02-24 17:21:19 -0500

    enforcer now clean. Correcting a few paths.

--- ontolib-io/src/test/java/com/github/phenomics/ontolib/io/owl/OwlImmutableOntologyLoaderTest.java
+++ ontolib-io/src/test/java/com/github/phenomics/ontolib/io/owl/OwlImmutableOntologyLoaderTest.java
@@ -5,12 +5,12 @@ import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.io.IOException;
 
+import com.github.phenomics.ontolib.formats.generic.GenericTerm;
+import com.github.phenomics.ontolib.formats.generic.GenericTermRelation;
+import com.github.phenomics.ontolib.io.owl.generic.GenericOwlFactory;
 import org.junit.Test;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
-import com.github.phenomics.ontolib.formats.common.CommonTerm;
-import com.github.phenomics.ontolib.formats.common.CommonTermRelation;
-import com.github.phenomics.ontolib.io.owl.common.CommonOwlFactory;
 import com.github.phenomics.ontolib.ontology.data.ImmutableOntology;
 
 /**
@@ -20,14 +20,14 @@ import com.github.phenomics.ontolib.ontology.data.ImmutableOntology;
 public class OwlImmutableOntologyLoaderTest {
 	@Test
 	public void testLoader() {
-		// ncit_module.owl contains 6 classes where 2 classes are dummy ones. 
-		OwlImmutableOntologyLoader<CommonTerm, CommonTermRelation> loader = 
-				new OwlImmutableOntologyLoader<CommonTerm, CommonTermRelation>(new File("src/test/resources/ncit_module.owl"));
+		// ncit_module.owl contains 6 classes where 2 classes are dummy ones.
+		OwlImmutableOntologyLoader<GenericTerm, GenericTermRelation> loader =
+				new OwlImmutableOntologyLoader<GenericTerm, GenericTermRelation>(new File("src/test/resources/ncit_module.owl"));
 
 		try {
-			CommonOwlFactory cof = new CommonOwlFactory();
-			ImmutableOntology<CommonTerm, CommonTermRelation> ontology = loader.load(cof);
-			
+			GenericOwlFactory cof = new GenericOwlFactory();
+			ImmutableOntology<GenericTerm, GenericTermRelation> ontology = loader.load(cof);
+
 			assertEquals(
 					"ImmutableDirectedGraph [edgeLists={ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2852]=ImmutableVertexEdgeList [inEdges=[ImmutableEdge [source=ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919], dest=ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2852], id=1]], outEdges=[]], ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919]=ImmutableVertexEdgeList [inEdges=[], outEdges=[ImmutableEdge [source=ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919], dest=ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C48596], id=2], ImmutableEdge [source=ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919], dest=ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2852], id=1]]], ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C48596]=ImmutableVertexEdgeList [inEdges=[ImmutableEdge [source=ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919], dest=ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C48596], id=2]], outEdges=[]], ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C60312]=ImmutableVertexEdgeList [inEdges=[], outEdges=[]]}, edgeCount=2]",
 					ontology.getGraph().toString());
