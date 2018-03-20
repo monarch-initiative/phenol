32841da21c151008f947b1198b7cec9353ca66ff
HyeongSik Kim <yy20716@gmail.com>
2018-03-20 16:53:39 -0700

    Revised owl file loader and its testcases; Fixed typos

--- phenol-io/src/test/java/org/monarchinitiative/phenol/io/owl/OwlImmutableOntologyLoaderTest.java
+++ phenol-io/src/test/java/org/monarchinitiative/phenol/io/owl/OwlImmutableOntologyLoaderTest.java
@@ -1,17 +1,23 @@
 package org.monarchinitiative.phenol.io.owl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 
 import org.monarchinitiative.phenol.formats.generic.GenericTerm;
 import org.monarchinitiative.phenol.formats.generic.GenericRelationship;
+import org.monarchinitiative.phenol.formats.generic.GenericRelationshipType;
 import org.monarchinitiative.phenol.graph.IdLabeledEdge;
 import org.monarchinitiative.phenol.io.owl.generic.GenericOwlFactory;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.junit.Test;
 
 import org.monarchinitiative.phenol.ontology.data.ImmutableOntology;
+import org.monarchinitiative.phenol.ontology.data.ImmutableTermId;
+import org.monarchinitiative.phenol.ontology.data.ImmutableTermPrefix;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 
 /**
@@ -20,9 +26,9 @@ import org.monarchinitiative.phenol.ontology.data.TermId;
  * @author <a href="mailto:HyeongSikKim@lbl.gov">HyeongSik Kim</a>
  */
 public class OwlImmutableOntologyLoaderTest {
+
   @Test
-  public void testLoader() throws Exception {
-    // ncit_module.owl contains 6 classes where 2 classes are dummy ones.
+  public void testNCITLoad() throws Exception {
     final OwlImmutableOntologyLoader<GenericTerm, GenericRelationship> loader =
         new OwlImmutableOntologyLoader<GenericTerm, GenericRelationship>(
             new File("src/test/resources/ncit_module.owl"));
@@ -31,18 +37,46 @@ public class OwlImmutableOntologyLoaderTest {
     final ImmutableOntology<GenericTerm, GenericRelationship> ontology = loader.load(cof);
     final DefaultDirectedGraph<TermId, IdLabeledEdge> graph = ontology.getGraph();
 
-    assertEquals(
-        "([ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919], ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2852], ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C48596]], [(ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919] : ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2852])=(ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919],ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2852]), (ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919] : ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C48596])=(ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919],ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C48596])])",
-        graph.toString());
+    // 1. Checking vertices
+    // In this dummy ontology, we have 6 classes.
+    ImmutableTermPrefix termPrefix = new ImmutableTermPrefix("NCIT");
+    ImmutableTermId t1 = new ImmutableTermId(termPrefix, "C2919");
+    ImmutableTermId t2 = new ImmutableTermId(termPrefix, "C2852");
+    ImmutableTermId t3 = new ImmutableTermId(termPrefix, "C48596");
+    ImmutableTermId t4 = new ImmutableTermId(termPrefix, "C60312");
+    ImmutableTermId t5 = new ImmutableTermId(termPrefix, "C116977");
+    ImmutableTermId t6 = new ImmutableTermId(termPrefix, "C126659");
 
+    assertTrue(graph.vertexSet().contains(t1));
+    assertTrue(graph.vertexSet().contains(t2));
+    assertTrue(graph.vertexSet().contains(t3));
+    assertTrue(graph.vertexSet().contains(t4));
+    assertTrue(graph.vertexSet().contains(t5));
+    // This one is not recognized as a node due to the lack of rdfs:label.
+    assertFalse(graph.vertexSet().contains(t6));
+
+    // 2. Checking edges
+    // Two subclasses are translated into two edges. Other axioms are not yet captured.
     assertEquals(graph.edgeSet().size(), 2);
+    assertTrue(graph.getEdge(t1, t2) != null);
+    assertTrue(graph.getEdge(t1, t3) != null);
 
-    assertEquals(
-        "[ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C60312], ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C48596], ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919], ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2852]]",
-        ontology.getAllTermIds().toString());
+    // 3. Checking TermIds
+    // This is essentially the same as checking vertices.
+    assertTrue(ontology.getAllTermIds().contains(t1));
+    assertTrue(ontology.getAllTermIds().contains(t2));
+    assertTrue(ontology.getAllTermIds().contains(t3));
+    assertTrue(ontology.getAllTermIds().contains(t4));
+    assertTrue(ontology.getAllTermIds().contains(t5));
+    assertFalse(ontology.getAllTermIds().contains(t6));
 
-    assertEquals(
-        "{1=GenericRelationship [source=ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919], dest=ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2852], id=1, relationshipType=IS_A], 2=GenericRelationship [source=ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C2919], dest=ImmutableTermId [prefix=ImmutableTermPrefix [value=NCIT], id=C48596], id=2, relationshipType=IS_A]}",
-        ontology.getRelationMap().toString());
+    // 4. Checking RelationMap
+    // All meta-information on edges are available in RelationMap instance.
+    GenericRelationship gr1 = ontology.getRelationMap().get(graph.getEdge(t1, t2).getId());
+    GenericRelationship gr2 = ontology.getRelationMap().get(graph.getEdge(t1, t3).getId());
+    assertNotNull(gr1);
+    assertNotNull(gr2);
+    assertEquals(gr1.getRelationshipType(), GenericRelationshipType.IS_A);
+    assertEquals(gr2.getRelationshipType(), GenericRelationshipType.IS_A);
   }
 }
