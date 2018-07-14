2c683cd5d3657fd23c9e31c5857f8e1456d0bc22
pnrobinson <peter.robinson@charite.de>
2018-07-14 14:11:09 -0400

    Adding a few tests for HP annotation parser

--- phenol-io/src/test/java/org/monarchinitiative/phenol/io/obo/hpo/HpoAnnotationLineTest.java
+++ phenol-io/src/test/java/org/monarchinitiative/phenol/io/obo/hpo/HpoAnnotationLineTest.java
@@ -16,17 +16,16 @@ public class HpoAnnotationLineTest {
 
   private final static String EMPTY_STRING="";
 
-  private HpoAnnotationLine makeLine(String items[]) {
+  private HpoAnnotationLine makeLine(String items[]) throws PhenolException {
     String line = Arrays.stream(items).collect(Collectors.joining("\t"));
-    return new HpoAnnotationLine(line);
+    return HpoAnnotationLine.constructFromString(line);
   }
 
 
 
   @Test
   public void test1() throws PhenolException{
-    String items[]={"OMIM",
-      "269150",
+    String items[]={"OMIM:269150",
       "SCHINZEL-GIEDION MIDFACE RETRACTION SYNDROME",
       "",
       "HP:0030736",
@@ -37,13 +36,14 @@ public class HpoAnnotationLineTest {
       "",
       "",
       "P",
-      "2017-07-13",
-      "HPO:skoehler"};
+      "HPO:skoehler[2017-07-13]"};
     HpoAnnotationLine hpoAnnot = makeLine(items);
-    assertEquals("OMIM",hpoAnnot.getDatabase());
-    assertEquals("269150", hpoAnnot.getDBObjectId());
-    assertEquals("OMIM:269150", hpoAnnot.getDiseaseId());
+    assertEquals("OMIM:269150",hpoAnnot.getDiseaseId());
     assertEquals("SCHINZEL-GIEDION MIDFACE RETRACTION SYNDROME", hpoAnnot.getDbObjectName());
+    assertEquals("TAS",hpoAnnot.getEvidence());
+    assertEquals("OMIM:269150",hpoAnnot.getPublication());
+    assertEquals("P",hpoAnnot.getAspect());
+    assertEquals("HPO:skoehler[2017-07-13]", hpoAnnot.getBiocuration());
     assertFalse(hpoAnnot.isNOT());
     assertEquals(EMPTY_STRING,hpoAnnot.getFrequency());
     TermId diseaseId = hpoAnnot.getDiseaseTermId();
@@ -57,7 +57,7 @@ public class HpoAnnotationLineTest {
    * @throws PhenolException expected to throw because prefix is malformed
    */
   @Test(expected = PhenolException.class)
-  public void malformedDiseaseDatabasePrefix() throws PhenolException{
+  public void malformedDiseaseDatabasePrefix() throws PhenolException {
     String items[]={"BadPrefix",
       "269150",
       "SCHINZEL-GIEDION MIDFACE RETRACTION SYNDROME",
--- phenol-io/src/test/java/org/monarchinitiative/phenol/io/obo/hpo/HpoDiseaseAnnotationParserTest.java
+++ phenol-io/src/test/java/org/monarchinitiative/phenol/io/obo/hpo/HpoDiseaseAnnotationParserTest.java
@@ -1,32 +1,38 @@
 package org.monarchinitiative.phenol.io.obo.hpo;
 
-import com.google.common.collect.ImmutableCollection;
-import com.google.common.collect.ImmutableMultimap;
-import org.junit.Before;
-import org.junit.Rule;
+
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.monarchinitiative.phenol.base.PhenolException;
+import org.monarchinitiative.phenol.formats.hpo.HpoAnnotation;
 import org.monarchinitiative.phenol.formats.hpo.HpoDisease;
 import org.monarchinitiative.phenol.formats.hpo.HpoOntology;
 import org.monarchinitiative.phenol.io.utils.ResourceUtils;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.junit.rules.TemporaryFolder;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+/**
+ * These tests were constructed with a mini hpo file and a mini annotation file -- look at them to
+ * figure out the logic!
+ */
 public class HpoDiseaseAnnotationParserTest {
-  private HpoDiseaseAnnotationParser parser;
-  private Map<TermId, HpoDisease> parsedFile;
+  private static Map<TermId, HpoDisease> diseaseMap;
 
-  @Rule
-  public TemporaryFolder tmpFolder = new TemporaryFolder();
+  @ClassRule
+  public static TemporaryFolder tmpFolder = new TemporaryFolder();
 
-  @Before
-  public void setUp() throws IOException, PhenolException {
+  @BeforeClass
+  public static void setUp() throws IOException, PhenolException {
     System.setProperty("user.timezone", "UTC"); // Somehow setting in pom.xml does not work :(
     File hpoHeadFile;
 
@@ -37,23 +43,88 @@ public class HpoDiseaseAnnotationParserTest {
     final HpoOntology ontology = oboParser.parse();
 
     File hpoDiseaseAnnotationToyFile = tmpFolder.newFile("phenotype.100lines.hpoa.tmp");
-    ResourceUtils.copyResourceToFile("/phenotype.100lines.hpoa", hpoDiseaseAnnotationToyFile);
-    parser = new HpoDiseaseAnnotationParser(hpoDiseaseAnnotationToyFile.getAbsolutePath(), ontology);
-    parsedFile = parser.parse();
+    ResourceUtils.copyResourceToFile("/phenotype_annotation_head.tab", hpoDiseaseAnnotationToyFile);
+    HpoDiseaseAnnotationParser parser = new HpoDiseaseAnnotationParser(hpoDiseaseAnnotationToyFile.getAbsolutePath(), ontology);
+    diseaseMap = parser.parse();
+  }
+
+
+  @Test
+  public void testSizeOfDiseaseMap() {
+    assertEquals(diseaseMap.size(), 2);
+  }
+
+
+  @Test
+  public void testDiseaseOneInheritance() {
+    // Test that the parser correctly parses disease Id and number of annotations (4) of disease 1
+    HpoDisease testDisease = diseaseMap.get(TermId.constructWithPrefix("OMIM:123456"));
+    assertEquals(testDisease.getDiseaseDatabaseId(), TermId.constructWithPrefix("OMIM:123456"));
+    // This disease is annotated with Autosomal recessive inheritance HP:0000007 con
+    assertEquals(1,testDisease.getModesOfInheritance().size());
+    TermId autosomalRecessive = TermId.constructWithPrefix("HP:0000007");
+    assertEquals(autosomalRecessive,testDisease.getModesOfInheritance().get(0));
   }
 
   @Test
-  public void testParseHpoDiseaseAnnotation() throws PhenolException {
-    // Test that the parser correctly parses an hpoa file, and returns a map.
-    HpoDisease testDisease = parsedFile.get(TermId.constructWithPrefix("OMIM:147421"));
-    assertEquals(testDisease.getDiseaseDatabaseId(), TermId.constructWithPrefix("OMIM:147421"));
+  public void testDiseaseOneAnnotationCount() {
+    // Test that the parser correctly parses disease Id and number of annotations (4) of disease 1
+    HpoDisease testDisease = diseaseMap.get(TermId.constructWithPrefix("OMIM:123456"));
+    assertEquals(testDisease.getDiseaseDatabaseId(), TermId.constructWithPrefix("OMIM:123456"));
+    // This disease is annotated with Autosomal recessive inheritance HP:0000007 con
+    assertEquals(3,testDisease.getPhenotypicAbnormalities().size());
+    //Lacrimation abnormality HP:0000632
+    TermId lacrimation = TermId.constructWithPrefix("HP:0000632");
+    //Abnormal pupillary function HP:0007686
+    TermId pupillary = TermId.constructWithPrefix("HP:0007686");
+    // Microphthalmia HP:0000568
+    TermId microphthalmia = TermId.constructWithPrefix("HP:0000568");
+    List<TermId> abnormalities = testDisease.getPhenotypicAbnormalities().
+      stream().
+      map(HpoAnnotation::getTermId).
+      collect(Collectors.toList());
+    assertTrue(abnormalities.contains(lacrimation));
+    assertTrue(abnormalities.contains(pupillary));
+    assertTrue(abnormalities.contains(microphthalmia));
+    assertEquals(3,abnormalities.size());
   }
 
   @Test
-  public void testTermToDiseaseMap() {
-    ImmutableMultimap<TermId, TermId> phenotypeToDiseaseMap = parser.getTermToDiseaseMap();
-    ImmutableCollection<TermId> diseases = phenotypeToDiseaseMap.get(TermId.constructWithPrefix("HP:0000006"));
-    assertEquals(diseases.size(), 8);
+  public void testDiseaseTwoInheritance()  {
+    // Test that the parser correctly parses disease Id and number of annotations (3) of disease 1
+    HpoDisease testDisease = diseaseMap.get(TermId.constructWithPrefix("OMIM:654321"));
+    assertEquals(testDisease.getDiseaseDatabaseId(), TermId.constructWithPrefix("OMIM:654321"));
+    // This disease is annotated with Autosomal recessive inheritance HP:0000007 con
+    assertEquals(1,testDisease.getModesOfInheritance().size());
+    TermId autosomalDominant = TermId.constructWithPrefix("HP:0000006");
+    assertEquals(autosomalDominant,testDisease.getModesOfInheritance().get(0));
+  }
+
+  @Test
+  public void testDiseaseTwoAnnotations() {
+    // Test that the parser correctly parses disease Id and number of annotations (2) of disease 1
+    HpoDisease testDisease = diseaseMap.get(TermId.constructWithPrefix("OMIM:654321"));
+    assertEquals(testDisease.getDiseaseDatabaseId(), TermId.constructWithPrefix("OMIM:654321"));
+    // This disease is annotated with Autosomal recessive inheritance HP:0000007 con
+    assertEquals(2,testDisease.getPhenotypicAbnormalities().size());
+    //Lacrimation abnormality HP:0000632
+    TermId lacrimation = TermId.constructWithPrefix("HP:0000632");
+    //Abnormal pupillary function HP:0007686
+    TermId pupillary = TermId.constructWithPrefix("HP:0007686");
+    // Microphthalmia HP:0000568
+    TermId microphthalmia = TermId.constructWithPrefix("HP:0000568"); // a NOT for disease 2
+    List<TermId> abnormalities = testDisease.getPhenotypicAbnormalities().
+      stream().
+      map(HpoAnnotation::getTermId).
+      collect(Collectors.toList());
+    assertTrue(abnormalities.contains(lacrimation));
+    assertTrue(abnormalities.contains(pupillary));
+   // assertTrue(abnormalities.contains(microphthalmia));
+    assertEquals(2,abnormalities.size());
+
+    List<TermId> notAbnormal = testDisease.getNegativeAnnotations();
+    assertEquals(1,notAbnormal.size());
+    assertTrue(notAbnormal.contains(microphthalmia));
   }
 
 
