783321d3deba3587feec7d1e4397710b7083314c
pnrobinson <peter.robinson@jax.org>
2018-07-17 15:36:14 -0400

    Adding MpGene classes

---
+++ phenol-core/src/main/java/org/monarchinitiative/phenol/formats/mpo/MpMarkerType.java
@@ -0,0 +1,22 @@
+package org.monarchinitiative.phenol.formats.mpo;
+
+public enum MpMarkerType {
+
+  BAC_YAC_END("BAC/YAC end"),
+  COMPLEX_CLUSTER_REGION("Complex/Cluster/Region"),
+  CYTOGENETIC_MARKER("Cytogenetic Marker"),
+  DNA_SEGMENT("DNA Segment"),
+  GENE("Gene"),
+  OTHER_GENOME_FEATURE("Other Genome Feature"),
+  PSEUDOGENE("Pseudogene"),
+  QTL("QTL"),
+  TRANSGENE("Transgene");
+
+  private final String name;
+
+
+  MpMarkerType(String name) {
+    this.name=name;
+  }
+
+}
