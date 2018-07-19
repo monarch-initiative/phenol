ca3cf8917ab9680e24cfdf855a333d5cb9fd5a7b
Michael Gargano <Michael.Gargano@jax.org>
2018-07-19 14:29:00 -0400

    more cleanup

--- phenol-io/src/main/java/org/monarchinitiative/phenol/io/assoc/HpoAssociationParser.java
+++ phenol-io/src/main/java/org/monarchinitiative/phenol/io/assoc/HpoAssociationParser.java
@@ -7,8 +7,6 @@ import org.monarchinitiative.phenol.formats.hpo.*;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 import org.monarchinitiative.phenol.ontology.data.TermPrefix;
 import org.monarchinitiative.phenol.io.obo.hpo.HpoDiseaseAnnotationParser;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.*;
