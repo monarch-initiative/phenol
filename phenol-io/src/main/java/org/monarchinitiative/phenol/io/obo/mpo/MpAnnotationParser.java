587735b2e27c2760ee2a8e82e8755d28ca2c1ea7
pnrobinson <peter.robinson@jax.org>
2018-08-01 17:31:38 -0400

    more documentation

--- phenol-io/src/main/java/org/monarchinitiative/phenol/io/obo/mpo/MpAnnotationParser.java
+++ phenol-io/src/main/java/org/monarchinitiative/phenol/io/obo/mpo/MpAnnotationParser.java
@@ -10,8 +10,11 @@ import java.util.Map;
  * 1.  MGI_Strain.rpt
  * 2.  MGI_Nonstandard_Strain.rpt
  * 3.  MRK_List2.rpt  (Markers, i.e., mainly genes)
- * 4.  MGI_PhenoGenoMP.rpt
+ * 4.  MGI_GenePheno.rpt (for now, omit  MGI_PhenoGenoMP.rpt).
  * 5.  MGI_Pheno_Sex.rpt
+ * // Plan to have a parallel parser for http://www.mousephenotype.org/ data.
+ * // The MpAnnotation class needs to be flexible enough to work with both
+ * // c.f. HpAnnotation and HpDisease
  */
 public class MpAnnotationParser {
 
