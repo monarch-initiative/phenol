adf1b0a2f12a81e21364a742a231e5b4bd2c66b4
pnrobinson <annas0ph1e>
2018-08-24 11:56:15 -0400

    re-adding GoOntology

---
+++ phenol-core/src/main/java/org/monarchinitiative/phenol/formats/go/GoOntology.java
@@ -0,0 +1,68 @@
+package org.monarchinitiative.phenol.formats.go;
+
+import java.util.Collection;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.monarchinitiative.phenol.ontology.data.Relationship;
+import org.monarchinitiative.phenol.ontology.data.Term;
+import org.monarchinitiative.phenol.graph.IdLabeledEdge;
+import org.monarchinitiative.phenol.ontology.data.ImmutableOntology;
+import org.monarchinitiative.phenol.ontology.data.TermId;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSortedMap;
+
+/**
+ * Implementation of the GO as an {@link ImmutableOntology}.
+ *
+ * @author <a href="mailto:manuel.holtgrewe@bihealth.de">Manuel Holtgrewe</a>
+ * @author <a href="mailto:sebastian.koehler@charite.de">Sebastian Koehler</a>
+ */
+public final class GoOntology extends ImmutableOntology {
+
+  /** Serial UID for serialization. */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Constructor.
+   *
+   * @param metaInfo {@link ImmutableSortedMap} with meta information.
+   * @param graph Graph with the ontology's topology.
+   * @param rootTermId {@link TermId} of the root term.
+   * @param nonObsoleteTermIds {@link Collection} of {@link TermId}s of non-obsolete terms.
+   * @param obsoleteTermIds {@link Collection} of {@link TermId}s of obsolete terms.
+   * @param termMap Mapping from {@link TermId} to GO term.
+   * @param relationMap Mapping from numeric edge identifier to {@link Relationship}.
+   */
+  public GoOntology(
+    ImmutableSortedMap<String, String> metaInfo,
+    DefaultDirectedGraph<TermId, IdLabeledEdge> graph,
+    TermId rootTermId,
+    Collection<TermId> nonObsoleteTermIds,
+    Collection<TermId> obsoleteTermIds,
+    ImmutableMap<TermId, Term> termMap,
+    ImmutableMap<Integer, Relationship> relationMap) {
+    super(metaInfo, graph, rootTermId, nonObsoleteTermIds, obsoleteTermIds, termMap, relationMap);
+  }
+
+  @Override
+  public String toString() {
+    return "GoOntology [getMetaInfo()="
+      + getMetaInfo()
+      + ", getGraph()="
+      + getGraph()
+      + ", getTermMap()="
+      + getTermMap()
+      + ", getRelationMap()="
+      + getRelationMap()
+      + ", getRootTermId()="
+      + getRootTermId()
+      + ", getAllTermIds()="
+      + getAllTermIds()
+      + ", getTerms()="
+      + getTerms()
+      + ", getNonObsoleteTermIds()="
+      + getNonObsoleteTermIds()
+      + ", getObsoleteTermIds()="
+      + getObsoleteTermIds()
+      + "]";
+  }
+}
