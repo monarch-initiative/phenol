9d30c77554b185eb62d52e66c6d6b7fab601ce05
Hannah Blau <hannah.blau@jax.org>
2018-10-05 22:57:47 -0400

    Added methods to create sets of MpAnnotations by MpId and to locate child-ancestor pairs.

--- phenol-core/src/main/java/org/monarchinitiative/phenol/formats/mpo/MpGeneModel.java
+++ phenol-core/src/main/java/org/monarchinitiative/phenol/formats/mpo/MpGeneModel.java
@@ -3,26 +3,23 @@ package org.monarchinitiative.phenol.formats.mpo;
 import com.google.common.collect.ImmutableList;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 
-import java.util.List;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.stream.Collectors;
+import java.util.*;
 
 class MpGeneModel extends MpModel {
   private List<TermId> genotypes;
 
   MpGeneModel(TermId markerId, MpoOntology mpoOnt,
-                     MpSimpleModel... modelList) {
+              MpSimpleModel... modelList) {
     mpgmConstructor(markerId, mpoOnt, modelList);
   }
 
   MpGeneModel(TermId markerId, MpoOntology mpoOnt,
-                     List<MpSimpleModel> modelList) {
+              List<MpSimpleModel> modelList) {
     mpgmConstructor(markerId, mpoOnt, modelList.toArray(new MpSimpleModel[] {}));
   }
 
   /*
-   * All of the models share the same gene (that's why they all belong to the same MpGeneModel).
+   * All of the models share the same gene markerId (that's why they all belong to the same MpGeneModel).
    */
   private void mpgmConstructor(TermId markerId, MpoOntology mpo, MpSimpleModel[] models) {
     this.markerId = markerId;
@@ -33,73 +30,64 @@ class MpGeneModel extends MpModel {
 
   private List<MpAnnotation> mergeSimpleModels(MpoOntology mpo, MpSimpleModel[] models,
                                                ImmutableList.Builder<TermId> gbuilder) {
-    // cannot use builder for annots because might need to remove something from set
-    HashSet<MpAnnotation> annots = new HashSet<>();
+    HashMap<TermId, Set<MpAnnotation>> annotsByMpId = new HashMap<>();
+
     for (MpSimpleModel model : models) {
+      // remember genotype, record annotations according to their MpId
       gbuilder.add(model.getGenotypeId());
-      mergeAnnotations(mpo, annots, model.getPhenotypicAbnormalities());
+      for (MpAnnotation annot : model.getPhenotypicAbnormalities()) {
+        // skip over any negated annotations
+        if (!annot.isNegated()) {
+          TermId mpId = annot.getTermId();
+          Set<MpAnnotation> annotSet = annotsByMpId.get(mpId);
+          if (annotSet == null) {
+            annotSet = new HashSet<>();
+            annotSet.add(annot);                   // need to write equals, hashcode functions for MpAnnotation
+            annotsByMpId.put(mpId, annotSet);      // or maybe merge together all MpAnnotations for the same MpId?
+          } else {
+            annotSet.add(annot);
+          }
+        }
+      }
     }
+    Set<MpAnnotation> annots = mergeAnnotations(mpo, annotsByMpId);
     return ImmutableList.copyOf(annots);
   }
 
-  private void mergeAnnotations(MpoOntology mpo, Set<MpAnnotation> alreadySeen, List<MpAnnotation> newAnnotations) {
-    // to be added from newAnnotations to alreadySeen
-    Set<MpAnnotation> toBeAdded = new HashSet<>();
-    // to be removed from alreadySeen because some new annotation is better
-    Set<MpAnnotation> toBeRemoved = new HashSet<>();
-    // Filter out negated annotations
-    Set<MpAnnotation> newAnnots = newAnnotations.stream()
-      .filter(annot -> !annot.isNegated())
-      .collect(Collectors.toCollection(HashSet<MpAnnotation>::new));
-
-    // If no annotations seen previously, just keep everything that is left after filtering
-    if (alreadySeen.isEmpty()) {
-      alreadySeen.addAll(newAnnots);
-    } else {
-      // Compare the new annotations to what has already been seen, decide what to keep and what to toss
-      for (MpAnnotation newAnnot : newAnnots) {
-        TermId newTermId = newAnnot.getTermId();
-        Set<TermId> ancestors = mpo.getAncestorTermIds(newTermId);
-        ancestors.remove(newTermId);
-
-        for (MpAnnotation existing : alreadySeen) {
-          TermId existingTermId = existing.getTermId();
-          if (existingTermId.equals(newTermId)) {
-            // what to do if they are both sex specific, maybe for the same sex or maybe for different sexes?
-            if (newAnnot.sexSpecific() && !existing.sexSpecific()) {
-              toBeAdded.add(newAnnot);
-              toBeRemoved.add(existing);
-            }
-          }
-          else {
-            // check whether existingTermId is an ancestor of newTermId; keep child and toss the ancestor
-            if (ancestors.contains(existingTermId)) {
-              toBeAdded.add(newAnnot);
-              toBeRemoved.add(existing);
-            }
-          }
-// also need to check whether newTermId is ancestor of existingTermId
+  private Set<MpAnnotation> mergeAnnotations(MpoOntology mpo, HashMap<TermId, Set<MpAnnotation>> annotsByMpId) {
+    // Check mpIds for child-ancestor pairs; keep the child and transfer the parents' annotations to the child
+    LinkedList<TermId> mpIds = new LinkedList<>(annotsByMpId.keySet());
+    int index = 0;
+    while (index < mpIds.size()) {
+      TermId child = mpIds.get(index);
+      Set<TermId> ancestors = mpo.getAncestorTermIds(child, false);
+      // getAncestorTermIds method includes the starting vertex in the set of ancestors
+      ancestors.remove(child);
+      for (TermId anc : ancestors) {
+        int ancIndex = mpIds.indexOf(anc);
+        if (ancIndex > -1) {
+          // ancestor is among the mpIds associated with this gene
+          // merge ancestor's annotations into the child's
+          Set<MpAnnotation> ancAnnots = annotsByMpId.get(anc);
+          annotsByMpId.get(child).addAll(ancAnnots);
+          // and remove the ancestor from further consideration
+          mpIds.remove(ancIndex);
         }
       }
-      alreadySeen.removeAll(toBeRemoved);
-      alreadySeen.addAll(toBeAdded);
+      index++;
     }
+    // mpIds now contains the list of termIds for which we want to retain annotations. This will be smaller
+    // than the keySet of annotsByMpId if some ancestor termIds were eliminated.
+    Set<MpAnnotation> returnSet = new HashSet<>();
+    for (TermId mpId : mpIds) {
+      returnSet.addAll(annotsByMpId.get(mpId));
+    }
+    return returnSet;
   }
 
-  private void mergeAnnotation(Set alreadySeen, MpAnnotation newAnnotation) {
-    boolean returnValue = false;
-  }
-
-  /**
-   * TODO -- decide if List<MpSimpleModel> is better
-   * TODO -- construct MpGeneModel by merging the MpModels
-   * @param modlist
-   * @return
-
+  /*
   public static MpGeneModel fromMpModelList(MpSimpleModel... modlist) {
   return null;
   }
    */
-
-
 }
