eb3bd4e7874b8b2bfbaf6c10bd28d0912a308645
pnrobinson <peter.robinson@jax.org>
2018-10-11 18:20:30 -0400

    new unit test

---
+++ phenol-core/src/test/java/org/monarchinitiative/phenol/formats/mpo/MpAnnotationTest.java
@@ -0,0 +1,26 @@
+package org.monarchinitiative.phenol.formats.mpo;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+import org.monarchinitiative.phenol.ontology.data.TermId;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class MpAnnotationTest {
+
+  /** Test the toString method -- todo, we should prettify this output! */
+  @Test
+  public void testAnnotationTypeString() {
+    TermId mpId = TermId.constructWithPrefix("MP:123");
+    List<String> pmids = ImmutableList.of();
+    MpAnnotation.Builder builder = new MpAnnotation.Builder(mpId,pmids).sex(MpSex.FEMALE);
+    MpAnnotation annot = builder.build();
+    String expected = "MP:123FEMALE_SPECIFIC";
+    assertEquals(expected,annot.toString());
+    //System.out.println("Annotation="+annot);
+  }
+
+
+}
