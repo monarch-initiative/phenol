89c5ac0212dc420e2db2a5599755d39af9fba376
pnrobinson <peter.robinson@jax.org>
2019-02-02 20:57:05 -0500

    adding more tests for frequency mergers

--- phenol-io/src/test/java/org/monarchinitiative/phenol/io/annotations/hpo/HpoAnnotationFileParserTest.java
+++ phenol-io/src/test/java/org/monarchinitiative/phenol/io/annotations/hpo/HpoAnnotationFileParserTest.java
@@ -1,5 +1,6 @@
 package org.monarchinitiative.phenol.io.annotations.hpo;
 
+import com.google.common.collect.ImmutableSet;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.monarchinitiative.phenol.annotations.hpo.HpoAnnotationEntry;
@@ -13,8 +14,11 @@ import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 /**
@@ -40,26 +44,46 @@ class HpoAnnotationFileParserTest {
   /**
    * The raw small file has three annotation lines: HP:0000006	Autosomal dominant inheritance
    * and HP:0000528	Anophthalmia (once with frequency 3/4, and once with frequency 2/5).
+   * There are also two annotations to HP:0007686	Abnormal pupillary function
    */
   @Test
   void testNumberOfAnnotationsPremerge() {
-    int expected=3;
+    int expected=7;
     assertEquals(expected,smallFile1.getEntryList().size());
   }
 
+  /**
+   * There are four unique HPO terms used in annotations.
+   */
   @Test
   void testNumberOfAnnotationsPostmerge() {
-    int expected=2;
+    int expected=4;
     HpoAnnotationModel mergedModel = smallFile1.getMergedModel();
     assertEquals(expected,mergedModel.getEntryList().size());
     // There were two entries for Anopthalmia, 3/4 and 2/5. Thus we expect 5/9
     String expectedFrequency = "5/9";
     TermId anopthalmia = TermId.of("HP:0000528");
     List<HpoAnnotationEntry> entryList =mergedModel.getEntryList();
+    HpoAnnotationEntry anophthalmiaEntry=null;
     for (HpoAnnotationEntry entry : entryList) {
       if (entry.getPhenotypeId().equals(anopthalmia)) {
-        assertEquals(expectedFrequency,entry.getFrequencyModifier());
+        anophthalmiaEntry=entry;
       }
+    }
+    assertNotNull(anophthalmiaEntry);
+    assertEquals(expectedFrequency,anophthalmiaEntry.getFrequencyModifier());
+    Set<String> expectedBiocuration= ImmutableSet.of("HP:probinson[2018-05-28]","HP:probinson[2019-05-28]");
+    String actualBiocuration=anophthalmiaEntry.getBiocuration();
+    String[] bioc=actualBiocuration.split(";");
+    for (String biocurationItem : bioc) {
+      assertTrue(expectedBiocuration.contains(biocurationItem));
+    }
+
+    Set<String> expectedPublication=ImmutableSet.of("PMID:3214","PMID:1234");
+    String actualPub=anophthalmiaEntry.getPublication();
+    String []pubs=actualPub.split(";");
+    for (String pub:pubs) {
+      assertTrue(expectedPublication.contains(pub));
     }
   }
 
--- phenol-io/src/test/java/org/monarchinitiative/phenol/io/annotations/hpo/HpoAnnotationModelTest.java
+++ phenol-io/src/test/java/org/monarchinitiative/phenol/io/annotations/hpo/HpoAnnotationModelTest.java
@@ -5,13 +5,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import org.monarchinitiative.phenol.annotations.hpo.HpoAnnotationEntry;
 import org.monarchinitiative.phenol.annotations.hpo.HpoAnnotationModel;
 import org.monarchinitiative.phenol.base.PhenolException;
 import org.monarchinitiative.phenol.io.OntologyLoader;
 import org.monarchinitiative.phenol.ontology.data.Ontology;
+import org.monarchinitiative.phenol.ontology.data.TermId;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -46,16 +49,57 @@ class HpoAnnotationModelTest {
         Assertions.assertFalse(v2sf.isDECIPHER());
     }
 
-    /** Our test file has three annotation lines. */
+    /** Our test file has seven annotation lines. */
     @Test
     void numberOfAnnotationsTest() {
-        Assertions.assertEquals(3,v2sf.getNumberOfAnnotations());
+        Assertions.assertEquals(7,v2sf.getNumberOfAnnotations());
     }
 
     @Test
   void testFrequencyMerge() {
      HpoAnnotationModel merged= v2sf.getMergedModel();
-     assertEquals(2,merged.getNumberOfAnnotations());
+     assertEquals(4,merged.getNumberOfAnnotations());
     }
+
+
+  @Test
+  void testMergeAbnormalPupillaryFunction() {
+    int expected=4;
+    HpoAnnotationModel mergedModel = v2sf.getMergedModel();
+    assertEquals(expected,mergedModel.getEntryList().size());
+    // There were two entries for Anopthalmia, 48% and 2/5. Thus we expect 5/10+2/5=7/15
+    //HP:0040282
+    String expectedFrequency = "7/15";
+    TermId abnormalPupillaryFunction = TermId.of("HP:0007686");
+    List<HpoAnnotationEntry> entryList =mergedModel.getEntryList();
+    for (HpoAnnotationEntry entry : entryList) {
+      if (entry.getPhenotypeId().equals(abnormalPupillaryFunction)) {
+        assertEquals(expectedFrequency,entry.getFrequencyModifier());
+      }
+    }
+  }
+
+  /**
+   * This tests whether we can merge a frequency from an HPO Frequency term
+   * HP:0040282 is "Frequent", defined as the mean of 30% and 79%
+   * thus 109/2=54.5, or 5/10 after rounding and normalizing to ten observations.
+   *
+   */
+  @Test
+  void testMergeLacrimationAbnormalityFunction() {
+    int expected = 3;
+    HpoAnnotationModel mergedModel = v2sf.getMergedModel();
+    // There were two entries for Lacrimation Abnormality, HP:0040282 and 12/42.
+    //HP:0040282 is frequent, 30% to 79, thus 109/2=54.5, or 5/10
+    // thus we have 12/42+5/10=17/52
+    String expectedFrequency = "17/52";
+    TermId LacrimationAbnormality = TermId.of("HP:0040282");
+    List<HpoAnnotationEntry> entryList =mergedModel.getEntryList();
+    for (HpoAnnotationEntry entry : entryList) {
+      if (entry.getPhenotypeId().equals(LacrimationAbnormality)) {
+        assertEquals(expectedFrequency,entry.getFrequencyModifier());
+      }
+    }
+  }
 
 }
