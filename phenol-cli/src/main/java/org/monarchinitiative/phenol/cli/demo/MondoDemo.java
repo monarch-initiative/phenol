9c649c46cbe6ba804d306da694a3f493ab4345ad
pnrobinson <peter.robinson@jax.org>
2019-07-01 08:42:19 -0400

    adding output to Mondo demo

--- phenol-cli/src/main/java/org/monarchinitiative/phenol/cli/demo/MondoDemo.java
+++ phenol-cli/src/main/java/org/monarchinitiative/phenol/cli/demo/MondoDemo.java
@@ -11,41 +11,47 @@ import org.monarchinitiative.phenol.ontology.data.Ontology;
 import org.monarchinitiative.phenol.ontology.data.Term;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 /**
  * A demonstration of how to access data in MONDO. We will extract a list of all OMIM diseases that are members of
  * a given phenoseries
+ *
  * @author <a href="mailto:peter.robinson@jax.org">Peter Robinson</a>
  */
 public class MondoDemo {
   private final String mondoPath;
+  private final String outPath;
 
   private Ontology mondo;
   private List<Term> phenoseriesRoots;
-  private Multimap<Term,TermId> phenoseries2omimMap;
+  private Multimap<Term, TermId> phenoseries2omimMap;
 
 
-  public MondoDemo(MondoDemo.Options options){
+  public MondoDemo(MondoDemo.Options options) {
     mondoPath = options.getMondoPath();
+    outPath = options.getOutPath();
     phenoseriesRoots = new ArrayList<>();
     phenoseries2omimMap = ArrayListMultimap.create();
   }
 
 
   // todo  -- also check if is exact match
-  boolean isOmimEntry(Term term) {
+  private boolean isOmimEntry(Term term) {
     for (Dbxref xref : term.getXrefs()) {
-      if (xref.getName().startsWith("OMIM") )
+      if (xref.getName().startsWith("OMIM"))
         return true;
     }
     return false;
   }
 
-  Optional<TermId> getOMIMid(Term term) {
+  private Optional<TermId> getOMIMid(Term term) {
     for (Dbxref xref : term.getXrefs()) {
-      if (xref.getName().startsWith("OMIM") && ! xref.getName().startsWith("OMIMPS"))
+      if (xref.getName().startsWith("OMIM") && !xref.getName().startsWith("OMIMPS"))
         return Optional.of(TermId.of(xref.getName()));
     }
     return Optional.empty();
@@ -54,13 +60,13 @@ public class MondoDemo {
 
   public void run() {
     File file = new File(mondoPath);
-    if (! file.exists()) {
+    if (!file.exists()) {
       System.err.print("[ERROR] Could not find mondo.obo at " + mondoPath);
       return;
     }
     mondo = OntologyLoader.loadOntology(file);
     Set<TermId> obsolete = mondo.getObsoleteTermIds();
-    for (Map.Entry<TermId,Term> entry  : mondo.getTermMap().entrySet()) {
+    for (Map.Entry<TermId, Term> entry : mondo.getTermMap().entrySet()) {
       if (obsolete.contains(entry.getKey())) {
         continue;
       }
@@ -72,35 +78,50 @@ public class MondoDemo {
       }
     }
     for (Term psterm : phenoseriesRoots) {
-      Set<TermId> members = OntologyAlgorithm.getDescendents(mondo,psterm.getId());
+      Set<TermId> members = OntologyAlgorithm.getDescendents(mondo, psterm.getId());
       for (TermId member : members) {
         Term candidate = mondo.getTermMap().get(member);
         if (isOmimEntry(candidate)) {
-          phenoseries2omimMap.put(psterm,candidate.getId());
+          phenoseries2omimMap.put(psterm, candidate.getId());
         }
       }
     }
-    for (Term psterm :  phenoseries2omimMap.keySet()) {
-      Collection<TermId> coll = phenoseries2omimMap.get(psterm);
-      for (TermId tid : coll) {
-        Term omimentry = mondo.getTermMap().get(tid);
-        Optional<TermId> omimIdopt = getOMIMid(omimentry);
-        if (omimIdopt.isPresent()) {
-          TermId omimId = omimIdopt.get();
-          System.out.println(psterm.getId().getValue() + "\t" +
-            psterm.getName() + "\t" +
-            omimId.getValue() + "\t" +
-            omimentry.getName());
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter(this.outPath));
+
+      for (Term psterm : phenoseries2omimMap.keySet()) {
+        Collection<TermId> coll = phenoseries2omimMap.get(psterm);
+        for (TermId tid : coll) {
+          Term omimentry = mondo.getTermMap().get(tid);
+          Optional<TermId> omimIdopt = getOMIMid(omimentry);
+          if (omimIdopt.isPresent()) {
+            TermId omimId = omimIdopt.get();
+            String line = psterm.getId().getValue() + "\t" +
+              psterm.getName() + "\t" +
+              omimId.getValue() + "\t" +
+              omimentry.getName();
+            System.out.println(line);
+            writer.write(line + "\n");
+          }
         }
       }
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 
   @Parameters(commandDescription = "MONDO demo")
   public static class Options {
-    @Parameter(names = {"-o","--obo"}, description = "path to mondo.obo file", required = true)
+    @Parameter(names = {"-m", "--mondo"}, description = "path to mondo.obo file", required = true)
     private String mondoPath;
+    @Parameter(names = {"-o", "--outfile"}, description = "name/path of output file", required = true)
+    private String outPath;
 
+
+    String getOutPath() {
+      return outPath;
+    }
 
     String getMondoPath() {
       return mondoPath;
