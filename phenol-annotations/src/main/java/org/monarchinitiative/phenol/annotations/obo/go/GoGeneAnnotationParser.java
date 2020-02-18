b70976e7092c195fcd9495fbba8ad2f0c06f9a60
pnrobinson <peter.robinson@jax.org>
2020-02-18 17:49:16 -0500

    Refactored GoGeneAnnotationParser according to JOBJ feedback

--- phenol-annotations/src/main/java/org/monarchinitiative/phenol/annotations/obo/go/GoGeneAnnotationParser.java
+++ phenol-annotations/src/main/java/org/monarchinitiative/phenol/annotations/obo/go/GoGeneAnnotationParser.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,15 +23,8 @@ import org.monarchinitiative.phenol.ontology.data.TermAnnotation;
  * <p><b>Usage Example</b>
  *
  * <pre>
- * File inputFile = "goa_human.gaf";
- * try {
- *   GoGeneAnnotationFileParser parser = new GoGeneAnnotationFileParser(inputFile);
- *   List<GoGaf21Annotation> = parser.getAnnotations();
- *   ...
- *   }
- * } except (PhenolException e) {
- *   System.err.println("Problem reading from file.");
- * }
+ * String filename = "goa_human.gaf";
+ * List<GoGaf21Annotation> annots = GoGeneAnnotationParser.loadAnnotations(filename);
  * </pre>
  *
  * @author <a href="mailto:manuel.holtgrewe@bihealth.de">Manuel Holtgrewe</a>
@@ -45,32 +40,34 @@ public final class GoGeneAnnotationParser  {
 
   public static List<GoGaf21Annotation> loadAnnotations(String filename) {
     File f = new File(filename);
-    return GoGeneAnnotationParser.loadAnnotations(f);
+    return GoGeneAnnotationParser.loadAnnotations(f.toPath());
   }
 
   public static List<GoGaf21Annotation> loadAnnotations(File file) {
-    String nextLine;
-    ImmutableList.Builder<GoGaf21Annotation> builder = new ImmutableList.Builder<>();
-    try (BufferedReader br = new  BufferedReader(new FileReader(file))) {
-      nextLine =br.readLine();
-      // skip the comments, which start with exclamation marks
-      while (nextLine.isEmpty() || nextLine.startsWith("!")) {
-        nextLine = br.readLine();
-      }
-      do {
-        final String[] arry = nextLine.split("\t");
-        try {
-          GoGaf21Annotation annot = new GoGaf21Annotation(arry);
-          builder.add(annot);
-        } catch (PhenolException e) {
-          System.err.printf("[ERROR] Could not parse GoGaf line (%s): %s", nextLine, e.getMessage());
-          // just skip this error. Should actually never happen
-        }
-      } while ((nextLine = br.readLine()) != null);
-    } catch (IOException e) {
-      throw new PhenolRuntimeException(e.getMessage());
-    }
+    return GoGeneAnnotationParser.loadAnnotations(file.toPath());
+  }
 
+  public static List<GoGaf21Annotation> loadAnnotations(Path path) {
+    ImmutableList.Builder<GoGaf21Annotation> builder = new ImmutableList.Builder<>();
+    try (BufferedReader br = Files.newBufferedReader(path)) {
+      for (String line; (line = br.readLine()) != null; ) {
+        // skip the comments, which start with exclamation marks
+        if (line.isEmpty() || line.startsWith("!")) {
+          // no-op
+        } else {
+          final String[] array = line.split("\t");
+          try {
+            GoGaf21Annotation annot = new GoGaf21Annotation(array);
+            builder.add(annot);
+          } catch (PhenolException e) {
+            System.err.printf("[ERROR] Could not parse GoGaf line (%s): %s", line, e.getMessage());
+            // just skip this error. Should actually never happen
+          }
+        }
+      }
+    } catch (IOException e) {
+      throw new PhenolRuntimeException("Could not parse " + path.toString() +": " + e.getMessage());
+    }
     return builder.build();
   }
 
@@ -79,14 +76,21 @@ public final class GoGeneAnnotationParser  {
    */
   public static List<TermAnnotation> loadTermAnnotations(String filename) {
     File f = new File(filename);
-    return GoGeneAnnotationParser.loadTermAnnotations(f);
+    return GoGeneAnnotationParser.loadTermAnnotations(f.toPath());
   }
 
   /**
    * @return Return a list of GoGafAnnotation as {@link TermAnnotation} objects.
    */
   public static List<TermAnnotation> loadTermAnnotations(File file) {
-    List<GoGaf21Annotation> annots = GoGeneAnnotationParser.loadAnnotations(file);
+   return GoGeneAnnotationParser.loadTermAnnotations(file.toPath());
+  }
+
+  /**
+   * @return Return a list of GoGafAnnotation as {@link TermAnnotation} objects.
+   */
+  public static List<TermAnnotation> loadTermAnnotations(Path path) {
+    List<GoGaf21Annotation> annots = GoGeneAnnotationParser.loadAnnotations(path);
     return new ArrayList<>(annots);
   }
 
