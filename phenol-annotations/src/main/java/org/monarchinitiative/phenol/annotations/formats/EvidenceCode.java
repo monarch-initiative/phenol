01beee485446310f57e356ee1e38b705cf6eac8c
Peter Robinson <peter.robinson@jax.org>
2020-10-21 11:35:39 -0400

    guarding against null ptr

--- phenol-annotations/src/main/java/org/monarchinitiative/phenol/annotations/formats/EvidenceCode.java
+++ phenol-annotations/src/main/java/org/monarchinitiative/phenol/annotations/formats/EvidenceCode.java
@@ -3,7 +3,15 @@ package org.monarchinitiative.phenol.annotations.formats;
 public enum EvidenceCode {
   IEA, TAS, PCS;
 
+  /**
+   * Return an enum to represent one of the HPO evidence codes. It should never happen
+   * that we get a null pointer for code because of the Q/C of that annotation files,
+   * but if this does happen we return the default code IEA.
+   * @param code
+   * @return an evidence code enum constant representing the evidence for a phenotype annotation
+   */
   public static EvidenceCode fromString(String code) {
+    if (code == null) return IEA;
     switch(code.toUpperCase()) {
       case "IEA":
         return IEA;
