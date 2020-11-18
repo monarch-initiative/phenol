67dedc87194c56a527eaa56309a5541e65f5dd08
Peter Robinson <peter.robinson@jax.org>
2020-11-18 16:56:21 -0500

    updating a few mappings

--- phenol-annotations/src/main/java/org/monarchinitiative/phenol/annotations/formats/hpo/maps/HpoTermsForMap.java
+++ phenol-annotations/src/main/java/org/monarchinitiative/phenol/annotations/formats/hpo/maps/HpoTermsForMap.java
@@ -11,7 +11,7 @@ public class HpoTermsForMap {
   protected static final TermId ABNORMAL_ASTROCYTE_MORPHOLOGY = TermId.of("HP:0100707");
   protected static final TermId ABNORMAL_B_CELL_MORPHOLOGY = TermId.of("HP:0002846");
   protected static final TermId ABNORMAL_BASOPHIL_MORPHOLOGY = TermId.of("HP:0001912");
-  protected static final TermId ABNORMAL_BREAST_MORPHOLOGY = TermId.of("HP:0031093");
+  protected static final TermId ABNORMALITY_BREAST = TermId.of("HP:0000769");
   protected static final TermId ABNORMAL_CHOROID_PLEXUS_MORPHOLOGY = TermId.of("HP:0007376");
   protected static final TermId ABNORMAL_CONJUNCTIVA_MORPHOLOGY = TermId.of("HP:0000502");
   protected static final TermId ABNORMAL_CORNEAL_STROMA_MORPHOLOGY = TermId.of("HP:0011492");
@@ -20,7 +20,7 @@ public class HpoTermsForMap {
   protected static final TermId ABNORMAL_IRIS_MORPHOLOGY = TermId.of("HP:0000525");
   protected static final TermId ABNORMAL_IRIS_PIGMENTATION = TermId.of("HP:0008034");
   protected static final TermId ABNORMAL_LENS_MORPHOLOGY = TermId.of("HP:0000517");
-  protected static final TermId ABNORMAL_LIVER_MORPHOLOGY = TermId.of("HP:0410042");
+  protected static final TermId ABNORMALITY_LIVER = TermId.of("HP:0001392");
   protected static final TermId ABNORMAL_MACROPHAGE_MORPHOLOGY = TermId.of("HP:0004311");
   protected static final TermId ABNORMAL_MAST_CELL_MORPHOLOGY = TermId.of("HP:0100494");
   protected static final TermId ABNORMAL_MONOCYTE_MORPHOLOGY = TermId.of("HP:0012144");
@@ -30,18 +30,18 @@ public class HpoTermsForMap {
   protected static final TermId ABNORMAL_PERIODONTIUM_MORPHOLOGY = TermId.of("HP:0410026");
   protected static final TermId ABNORMAL_PROSTATE_MORPHOLOGY = TermId.of("HP:0008775");
   protected static final TermId ABNORMAL_PULMONARY_ARTERY_MORPHOLOGY = TermId.of("HP:0030966");
-  protected static final TermId ABNORMAL_RENAL_MORPHOLOGY = TermId.of("HP:0012210");
+  protected static final TermId ABNORMALITY_KIDNEY = TermId.of("HP:0000077");
   protected static final TermId ABNORMAL_RESPIRATORY_EPITHELIUM_MORPHOLOGY = TermId.of("HP:0012253");
   protected static final TermId ABNORMAL_RETINAL_PIGMENTATION = TermId.of("HP:0007703");
-  protected static final TermId ABNORMAL_SKELETAL_MUSCLE_MORPHOLOGY = TermId.of("HP:0011805");
-  protected static final TermId ABNORMAL_SKIN_MORPHOLOGY = TermId.of("HP:0011121");
+  protected static final TermId ABNORMAL_MUSCULATURE = TermId.of(" HP:0003011");
+  protected static final TermId ABNORMALITY_SKIN = TermId.of("HP:0000951");
   protected static final TermId ABNORMAL_SYSTEMIC_ARTERIAL_MORPHOLOGY = TermId.of("HP:0011004");
   protected static final TermId ABNORMAL_T_CELL_MORPHOLOGY = TermId.of("HP:0002843");
   protected static final TermId ABNORMAL_TENDON_MORPHOLOGY = TermId.of("HP:0100261");
   protected static final TermId ABNORMAL_TRABECULAR_MESHWORK_MORPHOLOGY = TermId.of("HP:0012630");
   protected static final TermId ABNORMAL_TRACHEA_MORPHOLOGY = TermId.of("HP:0002778");
-  protected static final TermId ABNORMAL_UTERUS_MORPHOLOGY = TermId.of("HP:0031105");
-  protected static final TermId ABNORMAL_VASCULAR_MORPHOLOGY = TermId.of("HP:0025015");
+  protected static final TermId ABNORMALITY_UTERUS = TermId.of("HP:0000130");
+  protected static final TermId ABNORMALITY_VASCULATURE = TermId.of("HP:0002597");
 
 
 }
