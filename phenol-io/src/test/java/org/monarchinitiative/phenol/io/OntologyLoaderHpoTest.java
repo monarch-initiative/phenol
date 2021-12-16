653d21d73393239e18058ad2901906fee9809d10
Daniel Danis <daniel.danis@jax.org>
2021-12-16 18:51:57 +0100

    Add a toy HPO json ontology, reenable `OntologyLoaderHpoTest`

--- phenol-io/src/test/java/org/monarchinitiative/phenol/io/OntologyLoaderHpoTest.java
+++ phenol-io/src/test/java/org/monarchinitiative/phenol/io/OntologyLoaderHpoTest.java
@@ -1,8 +1,7 @@
 package org.monarchinitiative.phenol.io;
 
 
-
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.jgrapht.graph.DefaultDirectedGraph;
@@ -13,114 +12,111 @@ import org.monarchinitiative.phenol.ontology.data.TermId;
 import org.monarchinitiative.phenol.ontology.data.TermSynonym;
 
 
-
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasKey;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.monarchinitiative.phenol.ontology.data.TermSynonymScope.EXACT;
-import static org.monarchinitiative.phenol.ontology.data.TermSynonymScope.RELATED;
 
-@Disabled("OBO/OWL IO is not supported anymore")
 public class OntologyLoaderHpoTest {
-//  TODO Rework for JSON files
-//  private Ontology hpo;
-//
-//  public OntologyLoaderHpoTest() {
-//    this.hpo = OntologyLoader.loadOntology(Paths.get("src/test/resources/hp_head.obo").toFile());
-//  }
-//
-//  @Test
-//  public void testParseHpoHead() {
-//    final DefaultDirectedGraph<TermId, IdLabeledEdge> graph = hpo.getGraph();
-//    assertNotNull(graph);
-//  }
-//
-//  @Test
-//  public void testGetRightNumberOfTerms() {
-//    int expectedTermCount = 22; // there are 22 non-obsolete terms in hp_head.obo
-//    assertEquals(expectedTermCount, hpo.countAllTerms());
-//  }
-//
-//  @Test
-//  public void testGetRootTerm() {
-//    TermId rootId = TermId.of("HP:0000001");
-//    assertEquals(rootId, hpo.getRootTermId());
-//  }
-//
-//  @Test
-//  public void testGetNonRootTerms() {
-//    // outside of the root these four terms are in the hp_head file
-//    TermId tid1 = TermId.of("HP:0000005");
-//    TermId tid2 = TermId.of("HP:0000006");
-//    TermId tid3 = TermId.of("HP:0000007");
-//    TermId tid4 = TermId.of("HP:0000118");
-//    Map<TermId, Term> termMap = hpo.getTermMap();
-//    assertTrue(termMap.containsKey(tid1));
-//    assertTrue(termMap.containsKey(tid2));
-//    assertTrue(termMap.containsKey(tid3));
-//    assertTrue(termMap.containsKey(tid4));
-//    Term term1 = termMap.get(tid1);
-//    System.out.println(term1);
-//  }
-//
-//  @Test
-//  void ifHpoNotNull_thenOK() {
-//    assertNotNull(hpo);
-//  }
-//
-//  @Test
-//  void ifHpoHas22Terms_thenOK() {
-//    int expectedTermCount = 22; // hpo_small.obo
-//    assertEquals(expectedTermCount, hpo.countAllTerms());
-//  }
-//
-//  @Test
-//  void testModeOfInheritanceTerm() {
-//    TermId mode = TermId.of("HP:0000005");
-//    Term moiTerm = hpo.getTermMap().get(mode);
-//    assertNotNull(moiTerm);
-//    List<TermSynonym> synonyms =moiTerm.getSynonyms();
-//    // HP:0000005 has one EXACT synonym
-//    assertEquals(1, synonyms.size());
-//    TermSynonym syn = synonyms.get(0);
-//    assertEquals(EXACT,syn.getScope());
-//    assertFalse(syn.hasSynonymType());
-//  }
-//
-//
-//  @Test
-//  void trestAbnormalityOfTheEye() {
-//    TermId eyeId = TermId.of("HP:0000478");
-//    Term eye = hpo.getTermMap().get(eyeId);
-//    assertNotNull(eye);
-//    assertEquals("Abnormality of the eye", eye.getName());
-//    // There are three synonyms, all of them are layperson
-//    List<TermSynonym> synonyms =eye.getSynonyms();
-//    assertEquals(3, synonyms.size());
-//    Optional<TermSynonym> opt1 = synonyms.stream().filter(s -> s.getValue().equals("Abnormal eye")).findFirst();
-//    assertTrue(opt1.isPresent());
-//    TermSynonym syn1 = opt1.get();
-//    assertNotNull(syn1);
-//    assertEquals("Abnormal eye", syn1.getValue());
-//    assertEquals(EXACT, syn1.getScope());
-//    assertTrue(syn1.isLayperson());
-//    // "Abnormality of the eye"
-//    Optional<TermSynonym> opt2 = synonyms.stream().filter(s -> s.getValue().equals("Abnormality of the eye")).findFirst();
-//    assertTrue(opt2.isPresent());
-//    TermSynonym syn2 = opt2.get();
-//    assertNotNull(syn2);
-//    assertEquals(EXACT, syn2.getScope());
-//    assertTrue(syn2.isLayperson());
-//  //  "Eye disease"
-//    Optional<TermSynonym> opt3 = synonyms.stream().filter(s -> s.getValue().equals("Eye disease")).findFirst();
-//    assertTrue(opt3.isPresent());
-//    TermSynonym syn3 = opt3.get();
-//    assertNotNull(syn3);
-//    assertEquals(RELATED, syn3.getScope());
-//    assertTrue(syn3.isLayperson());
-//  }
+
+  private Ontology hpo;
+
+  @BeforeEach
+  public void beforeEach() {
+    hpo = OntologyLoader.loadOntology(Paths.get("src/test/resources/hpo_toy.json").toFile());
+  }
+
+  @Test
+  public void testParseHpoToy() {
+    final DefaultDirectedGraph<TermId, IdLabeledEdge> graph = hpo.getGraph();
+    assertNotNull(graph);
+  }
+
+  @Test
+  public void testGetRightNumberOfTerms() {
+    int expectedTermCount = 265; // there are 265 non-obsolete terms in hp_head.obo
+    assertEquals(expectedTermCount, hpo.countAllTerms());
+  }
+
+  @Test
+  public void testGetRootTerm() {
+    TermId rootId = TermId.of("HP:0000001");
+    assertEquals(rootId, hpo.getRootTermId());
+  }
+
+  @Test
+  public void testGetNonRootTerms() {
+    // outside the root there are some non-root terms in the ontology
+    Map<TermId, Term> termMap = hpo.getTermMap();
+    assertThat(termMap, hasKey(TermId.of("HP:0000006")));
+    assertThat(termMap, hasKey(TermId.of("HP:0000007")));
+    assertThat(termMap, hasKey(TermId.of("HP:0000118")));
+
+    Term modeOfInheritance = termMap.get(TermId.of("HP:0000005"));
+    assertThat(modeOfInheritance.getName(), is("Mode of inheritance"));
+  }
+
+  @Test
+  void ifHpoNotNull_thenOK() {
+    assertNotNull(hpo);
+  }
+
+  @Test
+  void ifHpoHas265Terms_thenOK() {
+    int expectedTermCount = 265; // hpo_toy.json
+    assertEquals(expectedTermCount, hpo.countAllTerms());
+  }
+
+  @Test
+  void testModeOfInheritanceTerm() {
+    TermId mode = TermId.of("HP:0000005");
+    Term moiTerm = hpo.getTermMap().get(mode);
+    assertNotNull(moiTerm);
+    List<TermSynonym> synonyms = moiTerm.getSynonyms();
+    // HP:0000005 has one EXACT synonym
+    assertEquals(1, synonyms.size());
+    TermSynonym syn = synonyms.get(0);
+    assertEquals(EXACT, syn.getScope());
+    assertFalse(syn.hasSynonymType());
+  }
+
+  @Test
+  void testArachnodactyly() {
+    TermId arachnodactylyId = TermId.of("HP:0001166");
+    Term arachnodactyly = hpo.getTermMap().get(arachnodactylyId);
+    assertNotNull(arachnodactyly);
+    assertEquals("Arachnodactyly", arachnodactyly.getName());
+    // There are three synonyms, two of them are layperson
+    List<TermSynonym> synonyms = arachnodactyly.getSynonyms();
+    List<String> synonymValues = synonyms.stream()
+      .map(TermSynonym::getValue)
+      .collect(Collectors.toList());
+    assertEquals(3, synonymValues.size());
+    assertThat(synonymValues, hasItems("Long slender fingers", "Spider fingers", "Long, slender fingers"));
+
+    TermSynonym syn1 = synonyms.get(0);
+    assertThat(syn1.getValue(), is("Long slender fingers"));
+    assertThat(syn1.getScope(), is(EXACT));
+    assertThat(syn1.isLayperson(), is(true));
+
+    // "Abnormality of the eye"
+    TermSynonym syn2 = synonyms.get(1);
+    assertThat(syn2.getValue(), is("Spider fingers"));
+    assertThat(syn2.getScope(), is(EXACT));
+    assertThat(syn2.isLayperson(), is(true));
+
+    //  "Eye disease"
+    TermSynonym syn3 = synonyms.get(2);
+    assertThat(syn3.getValue(), is("Long, slender fingers"));
+    assertThat(syn3.getScope(), is(EXACT));
+    assertThat(syn3.isLayperson(), is(false));
+  }
 
 }
