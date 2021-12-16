d6437d8c51ada7d620124028b7bfb061eaa60c96
Daniel Danis <daniel.danis@jax.org>
2021-12-16 22:35:41 +0100

    Test GO JSON loading

--- phenol-io/src/test/java/org/monarchinitiative/phenol/io/OntologyLoaderGoTest.java
+++ phenol-io/src/test/java/org/monarchinitiative/phenol/io/OntologyLoaderGoTest.java
@@ -1,6 +1,7 @@
 package org.monarchinitiative.phenol.io;
 
 import org.jgrapht.graph.DefaultDirectedGraph;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.monarchinitiative.phenol.base.PhenolException;
@@ -10,7 +11,9 @@ import org.monarchinitiative.phenol.ontology.data.Relationship;
 import org.monarchinitiative.phenol.ontology.data.Term;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.nio.file.Paths;
 import java.util.Map;
 
@@ -23,54 +26,58 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author <a href="mailto:HyeongSikKim@lbl.gov">HyeongSik Kim</a>
  * @author <a href="mailto:peter.robinson@jax.org">Peter Robinson</a>
  */
-@Disabled("OBO/OWL IO is not supported anymore")
-class OntologyLoaderGoTest {
-//  TODO Rework for JSON files
-//  private final Ontology ontology;
-//
-//  OntologyLoaderGoTest() throws PhenolException {
-//      this.ontology = OntologyLoader.loadOntology(Paths.get("src/test/resources/go/go_head.obo").toFile(), "GO");
-//  }
-//
-//  /** The obo file has the three top-level GO terms and one child each (i.e., three asserted is_a links).
-//   * The input of the GO file should create a new artificial root and should attach each of the top level terms,
-//   * and thus we expect 3+3=6 edges.
-//   */
-//  @Test
-//  void testEdgeSetSize() {
-//    DefaultDirectedGraph<TermId, IdLabeledEdge> graph = ontology.getGraph();
-//    assertEquals(6, graph.edgeSet().size());
-//  }
-//
-//  @Test
-//  void testArtificialRootTerm() {
-//    assertEquals(TermId.of("owl:Thing"), ontology.getRootTermId());
-//  }
-//
-//  /**
-//   * For local testing with the full Gene Ontology file. This does not need to run in a normal build
-//   * @throws FileNotFoundException if the test GO file cannot be found
-//   * @throws PhenolException upon parse errors
-//   */
-//  @Disabled
-//  @Test void testReal() throws Exception {
-//    String localpath = "src/test/resources/go.obo";
-//    String RO_PREFIX = "RO";
-//    Ontology goOntology = OntologyLoader.loadOntology(Paths.get(localpath).toFile(), "GO");
-//    assertNotNull(goOntology);
-//    Map<TermId, Term> termMap = goOntology.getTermMap();
-//    for (TermId tid : termMap.keySet()) {
-//      if (tid.getPrefix().equals(RO_PREFIX)) {
-//        System.err.println("FOUND " + tid.getPrefix());
-//        fail();
-//      }
-//    }
-//    Map<Integer, Relationship> relMap = ontology.getRelationMap();
-//    for (Relationship r : relMap.values()) {
-//      if (r.getSource().getPrefix().equals(RO_PREFIX)){
-//        System.err.println("FOUND2 " + r);
-//        fail();
-//      }
-//    }
-//  }
+public class OntologyLoaderGoTest {
+
+  private Ontology ontology;
+
+  @BeforeEach
+    public void beforeEach() throws Exception {
+    try (InputStream is = OntologyLoaderGoTest.class.getResourceAsStream("/go/go_head.json")) {
+      ontology = OntologyLoader.loadOntology(is);
+    }
+  }
+
+
+  /** The obo file has the three top-level GO terms and one child each (i.e., three asserted is_a links).
+   * The input of the GO file should create a new artificial root and should attach each of the top level terms,
+   * and thus we expect 3+3=6 edges.
+   */
+  @Test
+  public void testEdgeSetSize() {
+    DefaultDirectedGraph<TermId, IdLabeledEdge> graph = ontology.getGraph();
+    assertEquals(6, graph.edgeSet().size());
+  }
+
+  @Test
+  public void testArtificialRootTerm() {
+    assertEquals(TermId.of("owl:Thing"), ontology.getRootTermId());
+  }
+
+  /**
+   * For local testing with the full Gene Ontology file. This does not need to run in a normal build
+   * @throws FileNotFoundException if the test GO file cannot be found
+   * @throws PhenolException upon parse errors
+   */
+  @Disabled
+  @Test
+  public void testReal() throws Exception {
+    String localpath = "src/test/resources/go.obo";
+    String RO_PREFIX = "RO";
+    Ontology goOntology = OntologyLoader.loadOntology(Paths.get(localpath).toFile(), "GO");
+    assertNotNull(goOntology);
+    Map<TermId, Term> termMap = goOntology.getTermMap();
+    for (TermId tid : termMap.keySet()) {
+      if (tid.getPrefix().equals(RO_PREFIX)) {
+        System.err.println("FOUND " + tid.getPrefix());
+        fail();
+      }
+    }
+    Map<Integer, Relationship> relMap = ontology.getRelationMap();
+    for (Relationship r : relMap.values()) {
+      if (r.getSource().getPrefix().equals(RO_PREFIX)){
+        System.err.println("FOUND2 " + r);
+        fail();
+      }
+    }
+  }
 }
