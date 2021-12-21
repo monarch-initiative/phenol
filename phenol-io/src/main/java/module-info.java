3c8523c0eb2a3b73c13c6cac4ebc3c26cec9a7b3
Daniel Danis <daniel.danis@jax.org>
2021-12-21 15:59:10 +0100

    Add `module-info.java` to `phenol-io`

---
+++ phenol-io/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.monarchinitiative.phenol.io {
+  requires org.monarchinitiative.phenol.core;
+
+  requires obographs.core;
+  requires curie.util;
+  requires org.yaml.snakeyaml;
+  requires com.fasterxml.jackson.databind;
+  requires com.fasterxml.jackson.datatype.guava;
+  requires org.slf4j;
+}
