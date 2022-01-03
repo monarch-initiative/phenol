825537427fbada8600f4cb330fbf132a86de232f
Daniel Danis <daniel.danis@jax.org>
2022-01-03 21:55:08 +0100

    Add `module-info.java`

---
+++ phenol-analysis/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.monarchinitiative.phenol.analysis {
+  requires org.monarchinitiative.phenol.core;
+  requires org.monarchinitiative.phenol.annotations;
+
+  requires java.sql;
+  requires org.apache.commons.codec;
+  requires com.google.common;
+  requires org.slf4j;
+
+  // Exporting all packages as it's hard to say what the API should look like right now.
+  exports org.monarchinitiative.phenol.analysis;
+  exports org.monarchinitiative.phenol.analysis.mgsa;
+  exports org.monarchinitiative.phenol.analysis.scoredist;
+  exports org.monarchinitiative.phenol.analysis.stats;
+  exports org.monarchinitiative.phenol.analysis.stats.mtc;
+}
