74e02dac73cb0e5a10de3424037d8f8a732b76fe
Daniel Danis <daniel.danis@jax.org>
2022-01-19 15:29:56 +0100

    Export `utils` package from `phenol-core`

--- phenol-core/src/main/java/module-info.java
+++ phenol-core/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module org.monarchinitiative.phenol.core {
   exports org.monarchinitiative.phenol.ontology.algo;
   exports org.monarchinitiative.phenol.ontology.scoredist;
   exports org.monarchinitiative.phenol.ontology.similarity;
+  exports org.monarchinitiative.phenol.utils;
 
   requires transitive org.jgrapht.core; // due to DefaultDirectedGraph being exposed in MinimalOntology
   requires org.slf4j;
