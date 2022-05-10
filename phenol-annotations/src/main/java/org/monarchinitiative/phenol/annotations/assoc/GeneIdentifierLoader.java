2d562b0178fc229ed98190de0a216cd90bf55686
Daniel Danis <daniel.gordon.danis@protonmail.com>
2022-05-10 17:46:01 -0400

    Remove unused import.
    Signed-off-by: Daniel Danis <daniel.gordon.danis@protonmail.com>

--- phenol-annotations/src/main/java/org/monarchinitiative/phenol/annotations/assoc/GeneIdentifierLoader.java
+++ phenol-annotations/src/main/java/org/monarchinitiative/phenol/annotations/assoc/GeneIdentifierLoader.java
@@ -4,7 +4,6 @@ import org.monarchinitiative.phenol.annotations.formats.GeneIdentifiers;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 /**
