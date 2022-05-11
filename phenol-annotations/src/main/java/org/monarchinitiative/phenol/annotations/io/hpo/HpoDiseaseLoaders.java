f166ce00769d78159a6d04c331256faa924f30fe
Daniel Danis <daniel.gordon.danis@protonmail.com>
2022-05-11 11:00:15 -0400

    Add javadoc to `HpoDiseaseLoaders`.
    Signed-off-by: Daniel Danis <daniel.gordon.danis@protonmail.com>

--- phenol-annotations/src/main/java/org/monarchinitiative/phenol/annotations/io/hpo/HpoDiseaseLoaders.java
+++ phenol-annotations/src/main/java/org/monarchinitiative/phenol/annotations/io/hpo/HpoDiseaseLoaders.java
@@ -7,10 +7,20 @@ public class HpoDiseaseLoaders {
   private HpoDiseaseLoaders() {
   }
 
+  /**
+   * @param hpo HPO ontology.
+   * @param options loader options. <b>Note</b>: the default options are at {@link HpoDiseaseLoaderOptions#defaultOptions()}).
+   * @return default disease loader.
+   */
   public static HpoDiseaseLoader defaultLoader(Ontology hpo, HpoDiseaseLoaderOptions options) {
     return new HpoDiseaseLoaderDefault(hpo, options);
   }
 
+  /**
+   * @param hpo HPO ontology.
+   * @param options loader options. <b>Note</b>: the default options are at {@link HpoDiseaseLoaderOptions#defaultOptions()}.
+   * @return aggregated disease loader.
+   */
   public static HpoDiseaseLoader aggregated(Ontology hpo, HpoDiseaseLoaderOptions options) {
     return new AggregatedHpoDiseaseLoader(hpo, options);
   }
