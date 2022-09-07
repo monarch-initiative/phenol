8c04d8221ff22d34f4f6ea32bf0d9ec87c5ba85f
iimpulse <michael.gargano@jax.org>
2022-09-07 12:21:36 -0600

    load path for hpoa

--- phenol-annotations/src/main/java/org/monarchinitiative/phenol/annotations/io/hpo/HpoaDiseaseDataLoader.java
+++ phenol-annotations/src/main/java/org/monarchinitiative/phenol/annotations/io/hpo/HpoaDiseaseDataLoader.java
@@ -1,8 +1,14 @@
 package org.monarchinitiative.phenol.annotations.io.hpo;
 
+import org.monarchinitiative.phenol.annotations.formats.hpo.HpoDiseases;
+
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Set;
+import java.util.zip.GZIPInputStream;
 
 public interface HpoaDiseaseDataLoader {
 
@@ -11,5 +17,17 @@ public interface HpoaDiseaseDataLoader {
   }
 
   HpoaDiseaseDataContainer loadDiseaseData(InputStream is) throws IOException;
+
+  default HpoaDiseaseDataContainer loadDiseaseData(Path path) throws IOException {
+    try (InputStream is = openForReading(path)) {
+      return loadDiseaseData(is);}
+  }
+
+  private static BufferedInputStream openForReading(Path path) throws IOException {
+    InputStream is = Files.newInputStream(path);
+    if (path.toFile().getName().endsWith("gz"))
+      is = new GZIPInputStream(is);
+    return new BufferedInputStream(is);
+  }
 
 }
