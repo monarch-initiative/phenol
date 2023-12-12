4531e5234ec81478c513cf2efe152b7942f68996
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-12-12 14:28:09 -0500

    Audit `MicaCalculator`.

--- phenol-cli/src/main/java/org/monarchinitiative/phenol/cli/demo/MicaCalculator.java
+++ phenol-cli/src/main/java/org/monarchinitiative/phenol/cli/demo/MicaCalculator.java
@@ -3,34 +3,34 @@ package org.monarchinitiative.phenol.cli.demo;
 import org.monarchinitiative.phenol.annotations.constants.hpo.HpoSubOntologyRootTermIds;
 import org.monarchinitiative.phenol.annotations.formats.hpo.AnnotatedItem;
 import org.monarchinitiative.phenol.annotations.formats.hpo.AnnotatedItemContainer;
-import org.monarchinitiative.phenol.ontology.data.Identified;
-import org.monarchinitiative.phenol.ontology.data.Ontology;
-import org.monarchinitiative.phenol.ontology.data.TermId;
-import org.monarchinitiative.phenol.ontology.data.TermIds;
+import org.monarchinitiative.phenol.ontology.data.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
- * Class for calculating information content of the <em>most informative common ancestor</em> (MICA)
- * for {@link Ontology} concepts using a corpus of annotated.
+ * Calculate information content of the <em>most informative common ancestor</em> (IC<sub>MICA</sub>) for the concepts
+ * in given {@link MinimalOntology} using a corpus of annotated items.
+ * <p>
+ * For instance, calculate IC<sub>MICA</sub> of HPO terms that annotate Mendelian diseases.
+ * <p>
+ * The IC<sub>MICA</sub> values are in <em>nats</em>.
  */
 public class MicaCalculator {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MicaCalculator.class);
 
-  private final Ontology hpo;
+  private final MinimalOntology hpo;
+  /**
+   * {@code true} if the count of annotations that are not used to annotate at least one corpus item should be set to 1
+   * to prevent ic being {@link Double#POSITIVE_INFINITY}.
+   */
   private final boolean assumeAnnotated;
 
-  public MicaCalculator(Ontology hpo) {
-    this(hpo, false);
-  }
-
-  public MicaCalculator(Ontology hpo, boolean assumeAnnotated) {
+  public MicaCalculator(MinimalOntology hpo, boolean assumeAnnotated) {
     this.assumeAnnotated = assumeAnnotated;
     this.hpo = Objects.requireNonNull(hpo);
   }
@@ -40,24 +40,28 @@ public class MicaCalculator {
     Map<TermId, Integer> phenotypeIdToDiseaseIds = new HashMap<>();
     Map<TermId, Collection<TermId>> diseaseIdToTermIds = new HashMap<>();
 
+    Set<TermId> termsAndAncestorsBuilder = new HashSet<>();
     for (AnnotatedItem item: itemContainer) {
-      List<TermId> annotationTermIds = item.annotations().stream()
-        .map(Identified::id)
-        .collect(Collectors.toList());
-      // add term ancestors
-      Set<TermId> inclAncestorTermIds = TermIds.augmentWithAncestors(hpo, annotationTermIds, true);
+      for (Identified annotation : item.annotations()) {
+        TermId id = annotation.id();
+        for (TermId ancestor : hpo.graph().getAncestors(id, true)) {
+          termsAndAncestorsBuilder.add(ancestor);
+        }
+      }
 
-      for (TermId tid : inclAncestorTermIds) {
+      for (TermId tid : termsAndAncestorsBuilder) {
+        // We can't do this within the hot loop above because there is no guarantee of uniqueness of the ancestors.
         phenotypeIdToDiseaseIds.compute(tid, (key, val) -> val == null ? 0 : val + 1);
         diseaseIdToTermIds.computeIfAbsent(item.id(), key -> new HashSet<>()).add(tid); // Note that this MUST be a Set
       }
+      termsAndAncestorsBuilder.clear();
     }
 
     Map<TermId, Double> termToIc = new HashMap<>();
-    int totalPopulationHpoTerms = phenotypeIdToDiseaseIds.get(HpoSubOntologyRootTermIds.PHENOTYPIC_ABNORMALITY);
+    double totalPopulationHpoTerms = phenotypeIdToDiseaseIds.get(HpoSubOntologyRootTermIds.PHENOTYPIC_ABNORMALITY);
     for (Map.Entry<TermId, Integer> e : phenotypeIdToDiseaseIds.entrySet()) {
       int annotatedCount = assumeAnnotated ? Math.max(e.getValue(), 1) : e.getValue();
-      double ic = -1 * Math.log((double)annotatedCount/totalPopulationHpoTerms);
+      double ic = Math.log(totalPopulationHpoTerms / annotatedCount);
       termToIc.put(e.getKey(), ic);
     }
 
